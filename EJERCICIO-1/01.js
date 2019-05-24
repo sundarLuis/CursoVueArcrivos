653746205a6a598c31111ffd419f69b86ede33d7
luis romero <sundarjuegos@gmail.com>
2019-05-24 17:09:16 -0500

    ejercicio 5 clases css-2 con Vue.js

--- EJERCICIO-1/01.js
+++ EJERCICIO-1/01.js
@@ -1,7 +1,19 @@
 const app = new Vue({
     el: '#app',
     data:{
-       fondo:'bg-warning',
-       color:false
+      mensaje : 'Hola soy sundar',
+      contador : 0
+    },
+    computed:{
+        invertido(){
+            return this.mensaje.split('').reverse().join('');
+        },
+        color(){
+            return{
+                'bg-success':this.contador <= 10,
+                'bg-warning':this.contador > 10 && this.contador < 20,
+                'bg-danger':this.contador >= 20
+            }
+        }
     }
 })
