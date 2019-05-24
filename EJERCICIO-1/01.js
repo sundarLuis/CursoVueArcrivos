83b1fe1c67cca0d8c0e70bb62f79e92743b7f091
luis romero <sundarjuegos@gmail.com>
2019-05-24 16:17:15 -0500

    ejercicio 4 clases css con Vue.js

--- EJERCICIO-1/01.js
+++ EJERCICIO-1/01.js
@@ -1,30 +1,7 @@
 const app = new Vue({
     el: '#app',
     data:{
-        titulo:'hola mundo con Vue',
-        frutas:[
-            {nombre:'Pera', cantidad:10},
-            {nombre:'Platano',cantidad:10},
-            {nombre:'pera',cantidad:0}
-        ],
-        nuevaFruta: '',
-        total:0
-    },
-    methods:{
-        agregarFruta (){
-            this.frutas.push({
-               nombre: this.nuevaFruta,cantidad:0
-            }) ;
-            this.nuevaFruta = '';
-        }
-    },
-    computed:{
-       sumarFruta(){
-          this.total = 0;
-          for(fruta of this.frutas){
-              this.total = this.total + fruta.cantidad;
-          }
-          return this.total; 
-       }
+       fondo:'bg-warning',
+       color:false
     }
 })
