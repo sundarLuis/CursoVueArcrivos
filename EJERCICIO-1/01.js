1762f28dab9b841ecd7f5e87b3a8ed27649e319f
luis romero <sundarjuegos@gmail.com>
2019-05-24 15:22:39 -0500

    ejercicio 1

---
+++ EJERCICIO-1/01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        titulo:'hola mundo con Vue',
+        frutas:[
+            {nombre:'Pera', cantidad:10},
+            {nombre:'Platano',cantidad:10},
+            {nombre:'pera',cantidad:0}
+        ]
+    }
+})
