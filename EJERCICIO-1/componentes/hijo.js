e119406f533541487cfe54a4e032a7f5b0d91020
luis romero <sundarjuegos@gmail.com>
2019-05-24 23:19:35 -0500

    ejercicio 9 componentes basico 3 con Vue.js

--- EJERCICIO-1/componentes/hijo.js
+++ EJERCICIO-1/componentes/hijo.js
@@ -2,7 +2,16 @@ Vue.component('hijo',{
     template: //html
     ` <div class="py-5 bg-success p-3">
         <h4>Componente hijo: {{ numero }}</h4>
+        <h4>Nombre: {{nombre}}</h4>
     </div>
     `,
-    props:['numero']
+    props:['numero'],
+    data(){
+        return{
+            nombre:'Luis Romero'
+        }
+    },
+    mounted(){
+        this.$emit('nombreHijo',this.nombre);
+    }
 })
--- EJERCICIO-1/componentes/padre.js
+++ EJERCICIO-1/componentes/padre.js
@@ -3,12 +3,14 @@ Vue.component('padre',{
     `<div class="p-5 bg-dark text-white">
         <h2>Componente padre:{{numeroPadre}}</h2>
         <button class="btn btn-danger" @click="numeroPadre++">+</button>
-        <hijo :numero="numeroPadre"></hijo>
+        {{nombrePadre}}
+        <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
     </div>
     `,
     data(){
         return{
-            numeroPadre:0
+            numeroPadre:0,
+            nombrePadre:''
         }
     }
 })
