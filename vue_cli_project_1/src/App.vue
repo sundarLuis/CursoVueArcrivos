fa14727af513148ecad54a60bfd3cc7db6ce00e8
luis romero <sundarjuegos@gmail.com>
2019-05-26 13:21:45 -0500

    vue cli project 1 list frutas

---
+++ vue_cli_project_1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="container">
+    <Cabecera></Cabecera>
+    <Lista></Lista>
+  </div>
+</template>
+
+<script>
+import Cabecera from './components/Cabecera.vue'
+import Lista from './components/Lista.vue'
+export default {
+  name: 'app',
+  components: {
+    Cabecera,
+    Lista
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
